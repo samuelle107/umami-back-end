43ecd55d84ce5c58a200d8c185af9209a845164c
Samuel Le <samuelle107@gmail.com>
2021-01-31 18:47:18 -0600

    Add new tags to database

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const Recipe = require('../models/recipe');
+const CuisineTag = require('../models/cuisine-tag');
+const DietaryPreferenceTag = require('../models/dietary-preference-tag');
+const MealTag = require('../models/meal-tags');
 
 const router = express.Router();
 
@@ -27,6 +30,25 @@ router.post('/', async (req, res) => {
   try {
     const recipe = new Recipe(req.body);
     await recipe.save();
+
+    const { cuisine, dietaryPreferences, meal } = req.body.tags;
+
+    const addTagToDatabase = async (tag, Model) => {
+      const doesTagExist = await Model.exists({ tag });
+
+      if (!doesTagExist) {
+        await Model.create({ tag });
+      }
+
+      return -1;
+    };
+
+    await addTagToDatabase(cuisine, CuisineTag);
+    await addTagToDatabase(meal, MealTag);
+    await Promise.allSettled(dietaryPreferences.map((dietaryPreference) => addTagToDatabase(
+      dietaryPreference,
+      DietaryPreferenceTag,
+    )));
 
     res.send({ id: recipe.id });
   } catch (error) {
